a480f3441b7e48a1f2a8636836f991c97b3e375a
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-11-24 21:17:58 +0100

    added new function

---
+++ reverse_image_bing.js
@@ -0,0 +1,64 @@
+/**
+ * Reverse image search on Bing.
+ *
+ * @param item: key to stored image in s3
+ * @param options: Holds all configuration data and options
+ */
+async function Worker(key, options) {
+    results = {};
+
+    let image_path = await storage.storeFile(key);
+
+    await page.goto('https://www.bing.com/images?', { waitUntil: 'networkidle2' });
+      
+    await page.waitForSelector('#sb_sbi');
+
+    await page.click('#sb_sbi img');
+
+    await page.waitFor(500);
+
+    await page.waitForSelector('#sb_fileinput');
+
+    const input = await page.$('input#sb_fileinput');
+    await input.uploadFile(image_path);
+
+    await page.waitForNavigation();
+    await page.waitForSelector('#i_results');
+    await page.waitFor(500);
+
+    var image_data = await page.evaluate(() => {
+      
+      function get_imgurl(url) {
+          const regex = /mediaurl=(.*?)/gm;
+          let match = regex.exec(url);
+          if (match !== null) {
+              return decodeURIComponent(match[1]);
+          }
+      }
+      
+      let res = [];
+      let candidates = document.querySelectorAll('div.richImage') || [];
+      
+      console.log(candidates.length);
+      
+      for (let c of candidates) {
+        try {
+          let href = c.querySelector('.richImgLnk').getAttribute('href');
+          let text = c.querySelector('.captionContainer').innerText;
+          let imgrefurl = c.querySelector('.captionContainer a').getAttribute('href');
+          res.push({
+            href: href,
+            imgurl: get_imgurl(href),
+            imgrefurl: imgrefurl,
+            imgtext: text,
+          });
+        } catch (e) {
+          console.log(e.toString());
+        }
+      }
+      return res;
+    });
+
+    results[key] = image_data;
+    return results;
+}
---
+++ reverse_image_google.js
@@ -0,0 +1,92 @@
+/**
+ * Reverse image search on Google.
+ *
+ * @param item: key to stored image in s3
+ * @param options: Holds all configuration data and options
+ */
+async function Worker(key, options) {
+
+    let results = {};
+
+    let image_path = await storage.storeFile(key);
+
+    await page.goto('https://www.google.de/imghp?hl=de&tab=wi&ogbl', { waitUntil: 'networkidle2' });
+    
+    await page.waitForSelector('[aria-label="Bildersuche"]');
+    
+    await page.click('[aria-label="Bildersuche"]');
+    
+    await page.waitFor(500);
+    
+    await page.click('#qbug a');
+    await page.waitForSelector('#qbfile');
+    
+    const input = await page.$('input#qbfile');
+    await input.uploadFile(image_path);
+    
+    await page.waitForNavigation();
+    await page.waitForSelector('#rcnt');
+    await page.waitFor(500);
+
+    let content = await page.content();
+    
+    var data = await page.evaluate(() => {
+        return document.getElementById('res').innerText;
+    });
+    
+    // click on the link to get similar pictures
+    await page.click('g-section-with-header h3 > a');
+    await page.waitForNavigation();
+    await page.waitForSelector('#main');
+    await page.waitFor(500);
+    
+    var image_data = await page.evaluate(() => {
+      
+      function get_imgurl(url) {
+          const regex = /imgurl=(.*?)&/gm;
+          let match = regex.exec(url);
+          if (match !== null) {
+              return decodeURIComponent(match[1]);
+          }
+      }
+
+      function get_imgrefurl(url) {
+          const regex = /imgrefurl=(.*?)&/gm;
+          let match = regex.exec(url);
+          if (match !== null) {
+              return decodeURIComponent(match[1]);
+          }
+      }
+      
+      let res = [];
+      let candidates = document.querySelectorAll('.rg_bx > a') || [];
+      
+      if (candidates.length <= 0) {
+        candidates = document.querySelectorAll('[data-ri] > a');
+      }
+      
+      console.log(candidates.length);
+      
+      for (let c of candidates) {
+        try {
+          let href = c.getAttribute('href');
+          let imgurl = get_imgurl(href);
+          let imgrefurl = get_imgrefurl(href);
+          if (imgurl) {
+            res.push({
+              href: href,
+              imgurl: imgurl,
+              imgrefurl: imgrefurl,
+              imgtext: c.parentNode.innerText,
+            });
+          }
+        } catch (e) {
+          console.log(e.toString());
+        }
+      }
+      return res;
+    });
+
+    results[key] = image_data;
+    return results;
+}
