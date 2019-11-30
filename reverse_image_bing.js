9d6a5f7573beef49e955fb24dc293eb799b7a66e
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-11-30 19:31:00 +0100

    added some files

--- reverse_image_bing.js
+++ reverse_image_bing.js
@@ -10,31 +10,31 @@ class Render extends BrowserWorker {
 
     let results = {};
 
-    let image_path = await this.storage.getKey(key);
+    let image_path = await this.getKey(key);
 
     console.log(image_path);
 
-    await page.goto('https://www.bing.com/images?', { waitUntil: 'networkidle2' });
+    await this.page.goto('https://www.bing.com/images?', { waitUntil: 'networkidle2' });
       
-    await page.waitForSelector('#sb_sbi');
+    await this.page.waitForSelector('#sb_sbi');
 
-    await page.click('#sb_sbi img');
+    await this.page.click('#sb_sbi img');
 
-    await page.waitFor(500);
+    await this.page.waitFor(500);
 
-    await page.waitForSelector('#sb_fileinput');
+    await this.page.waitForSelector('#sb_fileinput');
 
-    const input = await page.$('input#sb_fileinput');
+    const input = await this.page.$('input#sb_fileinput');
     await input.uploadFile(image_path);
 
-    await page.waitForNavigation();
-    await page.waitForSelector('#i_results');
-    await page.waitFor(500);
+    await this.page.waitForNavigation();
+    await this.page.waitForSelector('#i_results');
+    await this.page.waitFor(500);
 
-    var image_data = await page.evaluate(() => {
+    var image_data = await this.page.evaluate(() => {
       
       function get_imgurl(url) {
-          const regex = /mediaurl=(.*?)/gm;
+          const regex = /mediaurl=(.*)/gm;
           let match = regex.exec(url);
           if (match !== null) {
               return decodeURIComponent(match[1]);
@@ -42,24 +42,22 @@ class Render extends BrowserWorker {
       }
       
       let res = [];
-      let candidates = document.querySelectorAll('div.richImage') || [];
+      let candidates = document.querySelectorAll('#i_results div.richImage') || [];
       
-      console.log(candidates.length);
-      
-      for (let c of candidates) {
+      for (let i = 0; i < candidates.length; i++) {
+        let c = candidates[i];
+        let obj = {rank: i+1};
+        
         try {
           let href = c.querySelector('.richImgLnk').getAttribute('href');
-          let text = c.querySelector('.captionContainer').innerText;
-          let imgrefurl = c.querySelector('.captionContainer a').getAttribute('href');
-          res.push({
-            //href: href,
-            imgurl: get_imgurl(href),
-            imgrefurl: imgrefurl,
-            imgtext: text,
-          });
+          obj.imgurl = get_imgurl(href);
+          obj.imgtext = c.querySelector('.captionContainer').innerText;
+          obj.imgrefurl = c.querySelector('.captionContainer a').getAttribute('href');
         } catch (e) {
           console.log(e.toString());
         }
+        
+        res.push(obj);
       }
       return res;
     });
