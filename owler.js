b949775f00dc379f2b98d840fb4a44380b4c6c0f
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-12-02 23:04:25 +0100

    update auto

--- owler.js
+++ owler.js
@@ -1,6 +1,6 @@
 class Owler extends BrowserWorker {
-
   async crawl(company_id) {
+    //await this.get_proxy({type: 'dedicated'});
     this.page.on('request', request => {
       let modified_request = {};
       // Inject post data into current request
@@ -10,20 +10,7 @@ class Owler extends BrowserWorker {
           components: ['company_info', 'ceo', 'top_competitors', 'keystats', 'cp'],
           section: 'cp'
         };
-        // https://stackoverflow.com/questions/53048414/making-a-post-request-using-puppeteer-with-json-payload
         modified_request.postData = JSON.stringify(mod_post);
-      }
-
-      // Override headers
-      const headers = Object.assign({}, request.headers(), {
-        'Origin': 'https://www.owler.com',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36',
-        'Cookie': cookie
-      });
-      //modified_request.headers = headers;
-
-      if (modified_request.postData) {
-        console.dir(modified_request, {depth: null, colors: true});
       }
       request.continue(modified_request);
     });
@@ -37,7 +24,6 @@ class Owler extends BrowserWorker {
     });
 
     await this.page.goto('https://www.owler.com/company/google');
-
     return result;
   }
 }
