b6b8b955d432de3e889d90bb67f4f1169f2475a5
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-12-21 18:36:28 +0100

    update auto

--- browser.js
+++ browser.js
@@ -1,14 +1,12 @@
 class Render extends BrowserWorker {
-
   async crawl(url) {
-
     await this.page.goto(url, {
       waitUntil: 'networkidle2', // two open connections is okay
-      timeout: 25000, // don't wait forever, it's better to return an error
     });
 
-    return await this.page.content();
-
+    return await this.clean_html({
+      tags: ['script', 'style'],
+      use_regex: false
+    });
   }
-
 }
