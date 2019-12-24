1aeb8b6c114fbe26ef05b1d34f3493507ac5474d
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-12-24 14:19:31 +0100

    update auto

--- http.js
+++ http.js
@@ -1,6 +1,6 @@
 class Get extends HttpWorker {
   async crawl(url) {
     let result = await this.Got(url);
-    return this.clean_html({tags: ['style', 'script']}, result.body);
+    return this.clean_html({tags: ['script']}, result.body);
   }
 }
