631f786e4916d7e708b0c9bbd5df527e455ffc41
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2019-12-30 19:18:44 +0100

    update auto

--- proxy_browser.js
+++ proxy_browser.js
@@ -5,6 +5,17 @@ class RenderProxy extends BrowserWorker {
         filter: { whitelisted: true, rotating: false, provider: 'cosmoproxy' },
         change: 1, // change proxy on failure or every 1th item
       });
+      
+    const referer = [
+      'https://www.google.com/',
+      'https://www.bing.com/',
+      'https://www.yahoo.com/',
+      'https://www.linkedin.com/',
+      'https://www.duckduckgo.com/',
+      'https://www.twitter.com/'
+    ];
+    const rand_referer = referer[Math.floor(Math.random() * referer.length)];
+    await page.setExtraHTTPHeaders({ 'referer': rand_referer });
 
     await this.page.goto(url, {
       waitUntil: 'networkidle2', // wait until there are maximally 2 connections left
