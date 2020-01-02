558e433a7564897949d82e1f063d3a0f01417d56
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2020-01-02 16:35:46 +0100

    update auto

--- render_lean.js
+++ render_lean.js
@@ -1,5 +1,4 @@
 class RenderProxyLean extends BrowserWorker {
-
   async crawl(url) {
     await this.get_proxy(
       {
@@ -9,26 +8,11 @@ class RenderProxyLean extends BrowserWorker {
 
     await this.page.goto(url, {
       waitUntil: 'networkidle2', // wait until there are maximally 2 connections left
+      timeout: 30000, // don't wait forever, it's better to fail than to consume too much resources
     });
 
-    // there must be a better way
-    await this.page.evaluate(() => {
-      let styles = document.getElementsByTagName('style');
-      for (let node of styles) {
-        node.parentNode.removeChild(node);
-      }
-
-      let scripts = document.getElementsByTagName('script');
-      for (let node of scripts) {
-        node.parentNode.removeChild(node);
-      }
-
-      let noscripts = document.getElementsByTagName('noscript');
-      for (let node of noscripts) {
-        node.parentNode.removeChild(node);
-      }
+    return await this.clean_html({
+      tags: ['script', 'style']
     });
-
-    return await this.page.content();
   }
 }
