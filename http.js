aa0e02f18c61e0e7bf565f410247cd90c54720d2
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2020-01-07 16:50:12 +0100

    update auto

--- http.js
+++ http.js
@@ -1,5 +1,11 @@
 class Get extends HttpWorker {
   async crawl(url) {
+    await this.get_proxy(
+      {
+        filter: { whitelisted: true, rotating: false, provider: 'cosmoproxy' }, // only proxies that are whitelisted
+        change: 10, // change proxy on failure or every nth item
+      });
+
     let result = await this.Got(encodeURI(url));
     return this.clean_html({tags: ['script']}, result.body);
   }
