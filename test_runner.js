9099b3c72b76c3c7c0ff5095bb4e3babf6dc5ce6
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2020-03-22 00:22:19 +0100

    update auto

--- test_runner.js
+++ test_runner.js
@@ -42,7 +42,7 @@ class BrowserWorker extends Worker {
     this.clipboardy = clipboardy;
     const puppeteer = require('puppeteer');
     this.browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       defaultViewport: null,
       args: ['--start-maximized'],
     });
