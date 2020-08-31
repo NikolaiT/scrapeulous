698221e620e70aab3a994ead1155f50672848a6f
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2020-09-01 00:32:54 +0200

    update auto

--- new_google_scraper.js
+++ new_google_scraper.js
@@ -57,7 +57,6 @@ class GoogleScraperNew {
           return text.slice(offset, text.indexOf('Time:'));
         });
         results.push({
-          text: await this.page.evaluate(() => { return document.body.innerText; }),
           status: 'Failed',
           error: 'Google recaptcha shown',
           blocked_ip: blocked_ip,
