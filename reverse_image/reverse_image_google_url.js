281dfa607f27f646090ef0e60050ca0491058d64
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2020-10-25 22:14:55 +0100

    update auto

--- reverse_image/reverse_image_google_url.js
+++ reverse_image/reverse_image_google_url.js
@@ -14,13 +14,13 @@ class ReverseImageGoogle {
     await this.page.type('input[name="image_url"]', url);
     await this.page.waitFor(50);
     await this.page.click('input[value="Search by image"]');
-    await this.page.waitForNavigation();
+    await this.page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 12000 });
     await this.page.waitForSelector('#rcnt');
     await this.page.waitFor(50);
     // click on the link to get similar pictures
     await this.page.click('title-with-lhs-icon > a', {timeout: 10000});
 
-    await this.page.waitForNavigation();
+    await this.page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 12000 });
     await this.page.waitForSelector('div[data-ri] a');
     await this.page.waitFor(150);
 
