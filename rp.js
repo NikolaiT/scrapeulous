9e983d95b38f56063230cbb0c28184b5c8d35e18
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2021-01-17 01:54:41 +0100

    update auto

--- rp.js
+++ rp.js
@@ -2,7 +2,7 @@ class Rp {
   async crawl(url) {
     await this.page.goto(url);
 
-    await this.waitFor(1000);
+    await this.page.waitFor(1000);
 
     const result = JSON.parse(await page.$eval('#result', el => el.textContent));
     
